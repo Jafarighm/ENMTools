a8127bc5932029bad6381a9b35898be135133ae1
Jafarighm <91380905+Jafarighm@users.noreply.github.com>
2022-07-17 22:39:20 +0430

    Delete 1 Installing ENMTools.R

--- 1 Installing ENMTools.R
+++
@@ -1,8 +0,0 @@
-
-install.packages("ENMTools")
-library(ENMTools)
-instal.extras()
-
-install.packages("devtools")
-library(devtools)
-devtools::instal_github(repo = "danlwarren/ENMTools", ref = "develop")
