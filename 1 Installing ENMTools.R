86d508244f645e5f7251cf70b51034ae15d7be4a
Jafarighm <91380905+Jafarighm@users.noreply.github.com>
2022-07-17 22:30:52 +0430

    Add files via upload

---
+++ 1 Installing ENMTools.R
@@ -0,0 +1,8 @@
+
+install.packages("ENMTools")
+library(ENMTools)
+instal.extras()
+
+install.packages("devtools")
+library(devtools)
+devtools::instal_github(repo = "danlwarren/ENMTools", ref = "develop")
