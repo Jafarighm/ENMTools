c7056684472eb23aa245b7178da35d32552285a0
Jafarighm <91380905+Jafarighm@users.noreply.github.com>
2022-07-17 23:20:36 +0430

    Add files via upload

---
+++ 1 Installing ENMTools.R
@@ -0,0 +1,16 @@
+
+# required package for "ENMTools": "raster", "sp", "dismo"
+#install.packages("raster")
+#install.packages("sp")
+#install.packages("dismo")
+
+install.packages("ENMTools")
+library(ENMTools)
+install.extras()
+
+# required package for "devtools": "usethis"
+#install.packages("usethis")
+
+install.packages("devtools")
+library(devtools)
+devtools::install_github(repo = "danlwarren/ENMTools", ref = "develop")
